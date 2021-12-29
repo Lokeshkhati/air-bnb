1824549cdfec36faf9ad418e5f2c87c77bc50e3a
LOKESH <lokeshkhati005@gmail.com>
2021-12-29 12:24:58 +0530

    completed

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,25 @@
 import React from "react";
 
-export default function Card({img,rating,reviewCount,location,title,price}) {
+export default function Card(props) {
+  let badgeText;
+  if(props.openSpots===0){
+    badgeText = "SOLD OUT"
+  }
+  else if(props.location ==="Online"){
+    badgeText = "ONLINE"
+  }
   return (
     <section className="card">
-      <img src={`../images/${img}`} className="card-image" />
+      {badgeText  && <div className="card-badge ">{badgeText}</div>}
+      <img src={`../images/${props.item.coverImg}`} className="card-image" />
       <div className="card-stats">
         <img src="../images/star.png" className="card-star" />
-        <span>{rating} </span>
-        <span className="gray" >{reviewCount} .</span>
-        <span  className="gray">{location} </span>
+        <span>{props.item.rating} </span>
+        <span className="gray" >{props.item.reviewCount} .</span>
+        <span  className="gray">{props.item.location} </span>
       </div>
-      <p>{title}</p>
-      <p> <strong>From ${price} </strong> / person</p> 
+      <p>{props.item.title}</p>
+      <p> <strong>From ${props.item.price} </strong> / person</p> 
     </section>
   );
 }
